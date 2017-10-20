886ec102abfc442685cefe71431f3f9afa43cbb0
Min Kim <mk4555@nyu.edu>
2017-10-20 21:12:26 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(cry="Squawk!") 
+def parrot(cry="Squawk!")
   puts "#{cry}"
-  return cry;
+  return cry
 end
